92e126da9a1315d216acdb7beb3cda3ebc196cf9
Dimitri DeFigueiredo <github@dimitri.xyz>
2018-12-06 18:14:54 -0200

    Organize tests & add error response test

--- test/Test.hs
+++ test/Test.hs
@@ -4,45 +4,30 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 import Data.Aeson
-import Data.ByteString.Lazy.Char8 (unpack)
 import Coinbene
 import Coins
 
-main = defaultMain $
-    testCase "\nExample test case" $ do
+main = defaultMain tests
 
+tests :: TestTree
+tests = testGroup "\nAPI test cases"
+  [ testCase "benchmark orderbook parsing" $ do
+        let mSampleResp = decode encodedSampleResponse
+        mSampleResp @?= Just sampleResponse
+
+  , testCase "benchmark failed response parsing" $ do
+        let mSampleError = decode encodedsampleRespError
+        mSampleError @?= Just sampleRespError
+
+  , testCase "live BTCBRL orderbook parsing" $ do
         (code, resp) <- getSecureURL (URL "https://api.coinbene.com/v1/market/orderbook?symbol=btcbrl&depth=200")
-        -- assertBool (show (code, response)) $ False
-
-        -- let mBRL = decode ("12345")
-        -- mBRL @?= Just (Price 12345 :: Price BRL)
-
-        let mResp = decode encodedSampleResponse
-        mResp @?= Just sampleResponse
-
+        let mResp :: Maybe (Resp QuoteBookPayload BRL BTC)
+            mResp =  decode resp
+        assertBool (show (code, resp)) (case mResp of {Nothing -> False; _ -> True})
+  ]
 
 ---------------------------------------
-sampleAsk :: AskQuote BRL BTC
-sampleAsk = AskQ { aqPrice = 20000, aqQuantity = 3}
-
-sampleBid :: BidQuote BRL BTC
-sampleBid = BidQ { bqPrice =  5000, bqQuantity = 2}
-
-sampleBook :: QuoteBook BRL BTC
-sampleBook = QuoteBook 
-    { qbAsks = [sampleAsk]
-    , qbBids = [sampleBid,sampleBid]
-    } 
-
----------------------------------------
-sampleRespError :: Resp QuoteBookPayload BRL BTC 
-sampleRespError = RespError
-    { rDescription = "some error"
-    , rTimestamp   = 1234
-    } 
-
----------------------------------------
--- Orderbook Parsing Test
+-- Benchmark orderbook parsing test
 sampleResponse :: Resp QuoteBookPayload BRL BTC
 sampleResponse = RespOK 
     { rPayload = BookPayload 
@@ -65,4 +50,27 @@ sampleResponse = RespOK
     }
 
 encodedSampleResponse = "{\"symbol\":\"BTCBRL\",\"orderbook\":{\"bids\":[{\"quantity\":0.4993,\"price\":14770},{\"quantity\":0.0313,\"price\":14750},{\"quantity\":0.0002,\"price\":14553.16}],\"asks\":[{\"quantity\":0.0028,\"price\":15556.57},{\"quantity\":0.1921,\"price\":16550},{\"quantity\":0.024700000000000000,\"price\":24299.99},{\"quantity\":0.000700000000000000,\"price\":28100}]},\"status\":\"ok\",\"timestamp\":1544017051349}"
+
 ---------------------------------------
+-- Error response parsing test
+sampleRespError :: Resp QuoteBookPayload BRL BTC 
+sampleRespError = RespError
+    { rDescription = "some error"
+    , rTimestamp   = 1234
+    } 
+
+encodedsampleRespError = "{\"description\":\"some error\",\"status\":\"error\",\"timestamp\":1234}"
+
+---------------------------------------
+sampleAsk :: AskQuote BRL BTC
+sampleAsk = AskQ { aqPrice = 20000, aqQuantity = 3}
+
+sampleBid :: BidQuote BRL BTC
+sampleBid = BidQ { bqPrice =  5000, bqQuantity = 2}
+
+sampleBook :: QuoteBook BRL BTC
+sampleBook = QuoteBook 
+    { qbAsks = [sampleAsk]
+    , qbBids = [sampleBid,sampleBid]
+    } 
+
