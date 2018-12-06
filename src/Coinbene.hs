66b7302ad3e3177a56090a8cc78a7169cbff8c04
Dimitri DeFigueiredo <github@dimitri.xyz>
2018-12-06 15:00:44 -0200

    WIP - Add orderbook parser
    Having numeric precision problems. Will fix them next.

--- src/Coinbene.hs
+++ src/Coinbene.hs
@@ -1,23 +1,149 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Coinbene where
 
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS      (tlsManagerSettings)
 import Network.HTTP.Types.Status    (statusCode)
-import Data.ByteString.Lazy.Char8   (unpack)
+import Data.ByteString.Lazy.Char8   (ByteString)
+
+import Data.Word                    (Word64)
+import Data.Char                    (toLower)
+import GHC.Generics
+import Data.Aeson
+import Data.Aeson.Types             (Object, Parser)
+import Market.Types                                   hiding (QuoteBook)
+
 
 newtype URL = URL {urlToString :: String} deriving (Show,Eq)
-
 -----------------------------------------
 -- Just Gets an httpS URL
 -- returns ( Response Code, Response Body)
 --
-getSecureURL :: URL -> IO (Int , String)
+getSecureURL :: URL -> IO (Int , ByteString)
 getSecureURL url = do
     req      <- parseUrlThrow (urlToString url)
     manager  <- newManager tlsManagerSettings
     response <- httpLbs (req {secure = True}) manager
-    return ( statusCode $ responseStatus response, unpack (responseBody response) )
+    return ( statusCode $ responseStatus response, responseBody response)
 -----------------------------------------
 
+newtype MilliEpoch = MilliEpoch Word64 deriving (Show, Eq, Ord, Generic, Num, Real, Enum, Integral)
+instance FromJSON MilliEpoch
+instance ToJSON   MilliEpoch
+
+data AskQuote p v
+  = AskQ
+    { aqPrice    :: Price p
+    , aqQuantity :: Vol   v
+    } deriving (Show, Eq, Generic)
+
+data BidQuote p v
+  = BidQ
+    { bqPrice    :: Price p
+    , bqQuantity :: Vol   v
+    } deriving (Show, Eq, Generic)
+
+data QuoteBook p v
+  = QuoteBook
+    { qbAsks::[AskQuote p v]
+    , qbBids::[BidQuote p v]
+    -- , qbTimestamp :: MilliEpoch
+    } deriving (Show, Eq, Generic)
+
+-----------------------------------------
+deriving instance Generic BRL
+instance FromJSON BRL
+instance ToJSON   BRL
+
+deriving instance Generic BTC
+instance FromJSON BTC
+instance ToJSON   BTC
+
+deriving instance Generic a => Generic (Price a)
+deriving instance Generic a => Generic (Vol   a)
+instance (Generic a, FromJSON a) => FromJSON (Price a)
+instance (Generic a, FromJSON a) => FromJSON (Vol   a)
+instance (Generic a, ToJSON   a) => ToJSON   (Price a)
+instance (Generic a, ToJSON   a) => ToJSON   (Vol   a)
+
+
+-- removes field label name mangling
+writeQuoteOpts = defaultOptions {fieldLabelModifier = map toLower . drop 2}
+------------
+-- instance (ToJSON (Price p), ToJSON (Vol v)) => ToJSON (AskQuote p v) where
+instance (ToJSON p, Generic p, ToJSON v, Generic v) => ToJSON (AskQuote p v) where
+    toJSON = genericToJSON writeQuoteOpts
+instance (ToJSON p, Generic p, ToJSON v, Generic v) => ToJSON (BidQuote p v) where
+    toJSON = genericToJSON writeQuoteOpts
+
+instance (FromJSON p, Generic p, FromJSON v, Generic v) => FromJSON (AskQuote p v) where
+    parseJSON = withObject "AskQuote" $ \v -> AskQ <$> v .: "price" <*> v .: "quantity"
+instance (FromJSON p, Generic p, FromJSON v, Generic v) => FromJSON (BidQuote p v) where
+    parseJSON = withObject "BidQuote" $ \v -> BidQ <$> v .: "price" <*> v .: "quantity"
+------------
+
+instance (ToJSON p, Generic p, ToJSON v, Generic v) => ToJSON (QuoteBook p v) where
+    toJSON = genericToJSON writeQuoteOpts
+instance (FromJSON p, Generic p, FromJSON v, Generic v) => FromJSON (QuoteBook p v) where
+    parseJSON = withObject "QuoteBook" $ \v -> QuoteBook <$> v .: "asks" <*> v .: "bids"
+-----------------------------------------
+
+data Resp tail p v
+  = RespOK
+    { rPayload :: tail p v
+    , rTimestamp :: MilliEpoch
+    }
+  | RespError
+    { rDescription :: String
+    , rTimestamp :: MilliEpoch
+    } 
+    deriving (Show, Eq, Generic)
+
+data QuoteBookPayload p v 
+  = BookPayload
+    { bpOrderbook :: QuoteBook p v
+    , bpSymbol :: String
+    } deriving (Show, Eq, Generic)
+
+data NoPayload a b = NoPayload deriving (Show, Eq, Generic)
+
+class ParsePayload a where
+    parsePayload :: Object -> Parser a
+
+instance ParsePayload (NoPayload a b) where
+    parsePayload _ = pure NoPayload 
+
+instance (FromJSON p, Generic p, FromJSON v, Generic v) => ParsePayload (QuoteBookPayload p v) where
+    parsePayload = parseQuoteBookPayload
+
+instance (FromJSON p
+         , Generic p
+         , FromJSON v
+         , Generic v
+         , ParsePayload (QuoteBookPayload p v)) 
+         => FromJSON (Resp QuoteBookPayload p v) where
+    parseJSON (Object h) = do
+        status <- h .: "status"
+        case (status :: String) of
+            "ok" -> RespOK
+                <$> parsePayload h
+                <*> h .: "timestamp"
+            "error" -> RespError
+                <$> h .: "description"
+                <*> h .: "timestamp"
+            _ -> fail ("Unknown response status code: " ++ status)
+
+
+parseQuoteBookPayload :: (FromJSON p, Generic p, FromJSON v, Generic v) => Object -> Parser (QuoteBookPayload p v)
+parseQuoteBookPayload h = do
+    mBook   <- h .:? "orderbook"
+    mSymbol <- h .:? "symbol"
+    case (mBook, mSymbol) of
+        (Just b, Just s) -> return (BookPayload b s)
+        _ -> fail "Unable to parse orderbook and symbol in successful response" 
--- test/Test.hs
+++ test/Test.hs
@@ -1,17 +1,111 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Test.Tasty
 import Test.Tasty.HUnit
 
+import Data.Aeson
+import Market.Types                         hiding (QuoteBook)
+import Data.ByteString.Lazy.Char8 (unpack)
 import Coinbene
 
 main = defaultMain $
-  testCase "\nExample test case" $ do
-    -- assertion no. 1 (passes)
-    2 + 2 @?= 4
+    testCase "\nExample test case" $ do
 
-    -- assertion no. 2 (fails)
-    (code, response) <- getSecureURL (URL "https://api.coinbene.com/v1/market/orderbook?symbol=btcbrl&depth=200")
-    assertBool (show (code, response)) $ False
+        (code, resp) <- getSecureURL (URL "https://api.coinbene.com/v1/market/orderbook?symbol=btcbrl&depth=200")
+        -- assertBool (show (code, response)) $ False
 
-    -- assertion no. 3 (would have failed, but won't be executed because
-    -- the previous assertion has already failed)
-    "foo" @?= "bar"
+        -- let mBRL = decode ("12345")
+        -- mBRL @?= Just (Price 12345 :: Price BRL)
+
+        let mResp = decode encodedResponse
+        mResp @?= Just decodedResponse
+
+
+---------------------------------------
+aq :: AskQuote BRL BTC
+aq = AskQ { aqPrice = 20000, aqQuantity = 3}
+
+bq :: BidQuote BRL BTC
+bq = BidQ { bqPrice =  5000, bqQuantity = 2}
+
+
+book :: QuoteBook BRL BTC
+book = QuoteBook 
+    { qbAsks = [aq]
+    , qbBids = [bq,bq]
+    } 
+
+response :: Resp QuoteBookPayload BRL BTC 
+response = RespOK
+    { rPayload = BookPayload book "ETHUSDT"
+    , rTimestamp = 1234
+    }
+
+encodedResponse = "{\"symbol\":\"BTCBRL\",\"orderbook\":{\"bids\":[{\"quantity\":0.4993,\"price\":14770},{\"quantity\":0.0313,\"price\":14750},{\"quantity\":0.0002,\"price\":14553.16},{\"quantity\":0.0114,\"price\":14500.01},{\"quantity\":0.0105,\"price\":14253.15},{\"quantity\":10.000000000000000000,\"price\":1000},{\"quantity\":0.065600000000000000,\"price\":2.36}],\"asks\":[{\"quantity\":0.0028,\"price\":15556.57},{\"quantity\":0.0004,\"price\":15570},{\"quantity\":0.1921,\"price\":16550},{\"quantity\":0.5060,\"price\":17000},{\"quantity\":1,\"price\":17350},{\"quantity\":1,\"price\":17700},{\"quantity\":1.866900000000000000,\"price\":21350},{\"quantity\":1.000000000000000000,\"price\":22776.29},{\"quantity\":0.024700000000000000,\"price\":24299.99},{\"quantity\":0.400000000000000000,\"price\":24350},{\"quantity\":0.014500000000000000,\"price\":24750},{\"quantity\":0.002400000000000000,\"price\":25000},{\"quantity\":0.38,\"price\":25200},{\"quantity\":0.000700000000000000,\"price\":28100}]},\"status\":\"ok\",\"timestamp\":1544017051349}"
+
+  --  "{\"symbol\":\"BTCBRL\"
+  --   ,\"orderbook\":
+  --       {\"bids\":
+  --           [{\"quantity\":0.4993,\"price\":14770}
+  --           ,{\"quantity\":0.0313,\"price\":14750}
+  --           ,{\"quantity\":0.0002,\"price\":14553.16}
+  --           ,{\"quantity\":0.0114,\"price\":14500.01}
+  --           ,{\"quantity\":0.0105,\"price\":14253.15}
+  --           ,{\"quantity\":10.000000000000000000,\"price\":1000}
+  --           ,{\"quantity\":0.065600000000000000,\"price\":2.36}
+  --           ]
+  --       ,\"asks\":
+  --           [{\"quantity\":0.0028,\"price\":15556.57}
+  --           ,{\"quantity\":0.0004,\"price\":15570}
+  --           ,{\"quantity\":0.1921,\"price\":16550}
+  --           ,{\"quantity\":0.5060,\"price\":17000}
+  --           ,{\"quantity\":1,\"price\":17350}
+  --           ,{\"quantity\":1,\"price\":17700}
+  --           ,{\"quantity\":1.866900000000000000,\"price\":21350}
+  --           ,{\"quantity\":1.000000000000000000,\"price\":22776.29}
+  --           ,{\"quantity\":0.024700000000000000,\"price\":24299.99}
+  --           ,{\"quantity\":0.400000000000000000,\"price\":24350}
+  --           ,{\"quantity\":0.014500000000000000,\"price\":24750}
+  --           ,{\"quantity\":0.002400000000000000,\"price\":25000}
+  --           ,{\"quantity\":0.38,\"price\":25200}
+  --           ,{\"quantity\":0.000700000000000000,\"price\":28100}
+  --           ]
+  --       }
+  --   ,\"status\":\"ok\"
+  --   ,\"timestamp\":1544017051349
+  --   }"
+
+decodedResponse :: Resp QuoteBookPayload BRL BTC
+decodedResponse = RespOK 
+    { rPayload = BookPayload 
+      { bpOrderbook = QuoteBook
+          { qbBids = 
+              [ BidQ {bqPrice = Price 14770.00000, bqQuantity = Vol 0.49930000}
+              , BidQ {bqPrice = Price 14750.00000, bqQuantity = Vol 0.03130000}
+              , BidQ {bqPrice = Price 14553.15999, bqQuantity = Vol 0.00020000} -- 
+              , BidQ {bqPrice = Price 14500.01000, bqQuantity = Vol 0.01140000}
+              , BidQ {bqPrice = Price 14253.14999, bqQuantity = Vol 0.01050000} -- 
+              , BidQ {bqPrice = Price 1000.00000, bqQuantity = Vol 10.00000000}
+              , BidQ {bqPrice = Price 2.35999,     bqQuantity = Vol 0.06560000} --
+              ]
+          , qbAsks = 
+              [ AskQ {aqPrice = Price 15556.56999, aqQuantity = Vol 0.00279999} -- --
+              , AskQ {aqPrice = Price 15570.00000, aqQuantity = Vol 0.00040000}
+              , AskQ {aqPrice = Price 16550.00000, aqQuantity = Vol 0.19209999}    --
+              , AskQ {aqPrice = Price 17000.00000, aqQuantity = Vol 0.50600000}
+              , AskQ {aqPrice = Price 17350.00000, aqQuantity = Vol 1.00000000}
+              , AskQ {aqPrice = Price 17700.00000, aqQuantity = Vol 1.00000000}
+              , AskQ {aqPrice = Price 21350.00000, aqQuantity = Vol 1.86690000}
+              , AskQ {aqPrice = Price 22776.29000, aqQuantity = Vol 1.00000000}
+              , AskQ {aqPrice = Price 24299.99000, aqQuantity = Vol 0.02469999}     --
+              , AskQ {aqPrice = Price 24350.00000, aqQuantity = Vol 0.40000000}
+              , AskQ {aqPrice = Price 24750.00000, aqQuantity = Vol 0.01450000}
+              , AskQ {aqPrice = Price 25000.00000, aqQuantity = Vol 0.00239999}     --
+              , AskQ {aqPrice = Price 25200.00000, aqQuantity = Vol 0.38000000}
+              , AskQ {aqPrice = Price 28100.00000, aqQuantity = Vol 0.00069999}     --
+              ]
+          }
+      , bpSymbol = "BTCBRL"
+      }
+  , rTimestamp = MilliEpoch 1544017051349
+  }
