feb9fe1c36aad32c19534830cb4565c4df94817e
Dimitri DeFigueiredo <github@dimitri.xyz>
2018-12-11 17:59:48 -0200

    WIP - Add Request logic & Cabal file
    There was bug in the .gitignore file that was excluding the cabal file.
    Should be good now. The MD5 "MAC" generation is still TBD.

--- src/Coinbene/Request.hs
+++ src/Coinbene/Request.hs
@@ -15,6 +15,9 @@ import Data.ByteString.Lazy.Char8   (ByteString, unpack)
 import Data.ByteString.Char8        (pack)
 import Data.Aeson
 
+import Control.Monad.Time           (MonadTime, currentTime)
+import Data.Time.Clock.POSIX        (utcTimeToPOSIXSeconds)
+
 import Coinbene
 import Coinbene.Parse
 
@@ -25,53 +28,108 @@ class Monad m => HTTP m where
 instance HTTP IO where
     http = httpLbs
 
-
-data Confirmation
+newtype OrderID = OID String
 data OrderSide = Bid | Ask
 
+type Confirmation = OrderID
+
 class Exchange config m where
-    getManager :: config -> m Manager
-    placeLimit :: ( HTTP m, CoinSymbol p, CoinSymbol v) => config -> OrderSide -> Price p -> Vol v -> m Confirmation
-    getBook    :: ( HTTP m, CoinSymbol p, CoinSymbol v) => config              -> Price p -> Vol v -> m (QuoteBook p v)
+    placeLimit :: ( HTTP m, MonadTime m, CoinSymbol p, CoinSymbol v) => config -> OrderSide -> Price p -> Vol v -> m Confirmation
+    getBook    :: ( HTTP m, MonadTime m, CoinSymbol p, CoinSymbol v) => config              -> Price p -> Vol v -> m (QuoteBook p v)
 
 
-data API_ID
+data API_ID = API_ID
 data API_KEY 
-
 data Coinbene = Coinbene
-    { cGetManager :: Manager
-    , cGetAPIID   :: API_ID
-    , cGetAPIKEY  :: API_KEY
+    { getManager :: Manager
+    , getAPI_ID  :: API_ID
+    , getAPI_KEY :: API_KEY
     }
 
 instance Exchange Coinbene IO where
-    getManager = pure . cGetManager
-    placeLimit = undefined
+    placeLimit = placeCoinbeneLimit
     getBook    = getCoinbeneBook
 
 
-getCoinbeneBook
-    :: forall m p v conf. 
-    ( HTTP m
-    , Exchange conf m
-    , CoinSymbol p
-    , CoinSymbol v
-    )
-    => conf -> Price p -> Vol v -> m (QuoteBook p v)
-getCoinbeneBook conf p v = do
-    let marketName = coinSymbol (undefined :: v) ++ coinSymbol (undefined :: p) 
-        request
-            = setRequestMethod "GET"
-            $ setRequestHost "api.coinbene.com"
-            $ setRequestPath "/v1/market/orderbook"
-            $ setRequestQueryString [("symbol", Just $ pack $ marketName),("depth", Just "200")]
-            $ setRequestSecure True
-            $ setRequestPort 443
-            $ defaultRequest
+-----------------------------------------
+marketSymbol :: forall p v. (CoinSymbol p, CoinSymbol v) => Price p -> Vol v -> String
+marketSymbol p v = coinSymbol (undefined :: v) ++ coinSymbol (undefined :: p)
 
-    manager  <- getManager conf
-    response <- http request manager
+coinbeneRequest
+    = setRequestHost "api.coinbene.com"
+    $ setRequestSecure True
+    $ setRequestPort 443
+    $ defaultRequest
 
+getCoinbeneBook :: forall m p v.
+    ( HTTP m, CoinSymbol p, CoinSymbol v)
+    => Coinbene -> Price p -> Vol v -> m (QuoteBook p v)
+getCoinbeneBook config p v = do
+    response <- http request (getManager config)
     case eitherDecode (responseBody response) of
-        Left errMsg -> error errMsg
+        Left errMsg -> error ("getCoinbeneBook: " ++ errMsg ++ " response: " ++ show response) -- FIX ME! should not use `error` here
         Right resp  -> return $ bpOrderbook $ rPayload resp
+  where
+    marketName = marketSymbol (undefined :: Price p) (undefined :: Vol v)
+    request
+        = setRequestMethod "GET"
+        $ setRequestPath "/v1/market/orderbook"
+        $ setRequestQueryString query
+        $ coinbeneRequest
+
+    query = map toQueryItem $
+        [ ("symbol", marketName)
+        , ("depth", "200")]
+
+placeCoinbeneLimit :: forall m p v.
+    ( HTTP m, MonadTime m, CoinSymbol p, CoinSymbol v)
+    => Coinbene -> OrderSide -> Price p -> Vol v -> m (Confirmation)
+placeCoinbeneLimit config side p v = do
+    signedReq <- signRequest (getAPI_ID config) (getAPI_KEY config) request
+    response <- http signedReq (getManager config)
+    return $ error ("placeCoinbeneLimit: " ++ " response: " ++ show response)
+  where
+    marketName = marketSymbol (undefined :: Price p) (undefined :: Vol v)
+    request
+        = setRequestMethod "POST"
+        $ setRequestPath "/v1/trade/order/place"
+        $ setRequestQueryString query
+        $ coinbeneRequest
+
+    query = map toQueryItem $
+        [ ("symbol", marketName)
+        , ("price", "22")
+        , ("quantity", "33")
+        , ("type", "buy-limit")
+        ]
+
+signRequest :: MonadTime m => API_ID -> API_KEY -> Request -> m Request
+signRequest id key request = do
+    now <- fmap (MilliEpoch . truncate . (1000*) . realToFrac . utcTimeToPOSIXSeconds) currentTime
+    let signedQuery = signQuery id key now (getRequestQueryString request)
+    return (setRequestQueryString signedQuery request)
+  where
+    signQuery :: API_ID -> API_KEY -> MilliEpoch -> Query -> Query
+    signQuery id key now query = undefined
+
+
+-- toQueryItem:: (ByteString, String) -> (ByteString, Maybe ByteString)
+toQueryItem (a, b) = (a, Just $ pack b)
+
+
+
+
+-- The items in the sign_list include the `apiid` and the `secret` key
+-- def sign(**kwargs):
+--     """
+--     将传入的参数生成列表形式，排序后用＆拼接成字符串，用hashbli加密成生sign
+--     """
+--     sign_list = []
+--     for key, value in kwargs.items():
+--         sign_list.append("{}={}".format(key, value))
+--     sign_list.sort()
+--     sign_str = "&".join(sign_list)
+--     mysecret = sign_str.upper().encode()
+--     m = hashlib.md5()
+--     m.update(mysecret)
+--     return m.hexdigest()
